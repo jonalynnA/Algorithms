3ac241e59bab0e6e7a260b35bcedf3691655f2a9
jonalynn masters <jonalynna.masters@gmail.com>
2020-04-15 22:17:23 -0700

    formatted print statement with " , " and colors

--- making_change/making_change.py
+++ making_change/making_change.py
@@ -1,17 +1,22 @@
 #!/usr/bin/python
 
 import sys
+import random
+
 
 def making_change(amount, denominations):
-  pass 
+  pass
 
 
 if __name__ == "__main__":
-  # Test our your implementation from the command line
-  # with `python making_change.py [amount]` with different amounts
-  if len(sys.argv) > 1:
-    denominations = [1, 5, 10, 25, 50]
-    amount = int(sys.argv[1])
-    print("There are {ways} ways to make {amount} cents.".format(ways=making_change(amount, denominations), amount=amount))
-  else:
-    print("Usage: making_change.py [amount]")
+    # Test our your implementation from the command line
+    # with `python making_change.py [amount]` with different amounts
+    amount = random.randrange(300)
+    if amount > 1:
+        denominations = [1, 5, 10, 25, 50]
+        amount = amount
+        ways = making_change(amount, denominations)
+        print("\nThere are \033[92m{:,d}\033[37m ways to make \033[92m{amount}\033[37m cents.\n".format(
+            ways, amount=amount))
+    else:
+        print("Usage: making_change.py [amount]")
