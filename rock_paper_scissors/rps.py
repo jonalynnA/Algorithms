0cf15736268e85183ae5643ea1cb191aaaa723f3
jonalynn masters <jonalynna.masters@gmail.com>
2020-04-17 09:33:24 -0700

    rock_paper_scissors complete

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -4,23 +4,20 @@ import sys
 import random
 
 
-def rock_paper_scissors(number_of_people):
+def rock_paper_scissors(number_of_players):
     global all_combinations
     all_combinations = []
-    choices_for_player = ['rock', 'paper', 'scissors']
+    move_options = ['rock', 'paper', 'scissors']
 
-    def possible_combination(players_left, possible_combinations):
-        if players_left == 0:
-            all_combinations.append(possible_combinations)
-            print(possible_combinations)
-            print(
-                players_left)
+    def build_moves(countdown, piece_being_built):
+        if countdown == 0:
+            all_combinations.append(piece_being_built)
             return
 
-        for choice in choices_for_player:
-            array_of_players_throw = possible_combinations + [choice]
-            possible_combination(num_of_people - 1, array_of_players_throw)
-    possible_combination(number_of_people, [])
+        for move in move_options:
+            new_piece_result = piece_being_built + [move]
+            build_moves(countdown - 1, new_piece_result)
+    build_moves(number_of_players, [])
 
     return all_combinations
 
@@ -32,9 +29,8 @@ if __name__ == "__main__":
         num_of_people = num_of_people
         print(f"\nIf you were to play with \033[34m{num_of_people} people,\033[37m the possible combinations would be: \n\n", rock_paper_scissors(
             num_of_people))
-
+        print("\nThat is a total of: \033[32m" +
+              str(len(all_combinations)) + " combinations\n")
     else:
         print(
-            f"\n\033[31mUsage: rps.py number of plays was: {num_of_people}. Must be greater than 2.\n")
-print(
-    "\nThat is a total of: \033[32m" + str(len(all_combinations)) + " combinations\n")
+            f"\n\033[31mUsage: rps.py number of players was: {num_of_people}. Must be greater than 2.\n")
